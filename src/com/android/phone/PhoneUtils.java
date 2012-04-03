2443aab023fd254533a2bf309caee4677347637d
Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange.com>
2012-04-03 14:08:43 +0200

    Fix issue #18053
    Removes useless confirmation after cancelling USSD response.
    
    http://code.google.com/p/android/issues/detail?id=18053
    
    Change-Id: I762a963da0f37929757c50ba39e01df2b03eb699
    gned-off-by: Cuihtlauac ALVARADO <cuihtlauac.alvarado@orange.com>

--- src/com/android/phone/PhoneUtils.java
+++ src/com/android/phone/PhoneUtils.java
@@ -994,7 +994,7 @@ public class PhoneUtils {
                 if (DBG) log("- using text from PENDING MMI message: '" + text + "'");
                 break;
             case CANCELLED:
-                text = context.getText(R.string.mmiCancelled);
+                text = null;
                 break;
             case COMPLETE:
                 if (app.getPUKEntryActivity() != null) {
@@ -1061,10 +1061,10 @@ public class PhoneUtils {
                     } catch (RemoteException e) {
                         mNwService = null;
                     }
-                    if (DBG) log("Extended NW displayMMIInitiate (" + text + ")");
-                    if (text == null || text.length() == 0)
-                        return;
                 }
+                if (DBG) log("Extended NW displayMMIInitiate (" + text + ")");
+                if (text == null || text.length() == 0)
+                    return;
 
                 // displaying system alert dialog on the screen instead of
                 // using another activity to display the message.  This
